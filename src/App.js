83186e5e3451f4b45c53e7fa7755a14f897dacdc
TanmayNankar <tanmaynankar03@gmail.com>
2021-07-27 15:27:23 +0530

    cc
    cc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
 
-function App() {
+function App() {  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+     //if we write class here not a classname then it will show error
+     //and this is not html .
+     //this is jsx-javascript xml language .   
+  
+  <div className="App"> 
+     {/* This is header injection */}
+      <Header/>
+          <h1>This is a React app.</h1> 
+          <p>This is a APp Component</p>
+    {/* This is footer injection */}
+      <Footer/>
+             </div> 
+              );
+            }
+ 
+ export default App
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+
+function Header() {  
+  return (  
+     <div className="App"> 
+          <h1>This is Header </h1> 
+          <p>This APpLicaTioN Header</p>
+             </div> 
+              );
+            }
+ 
+ export default Header;
+ 

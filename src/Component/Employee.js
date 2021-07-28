f955b5a07e19e24a80f236724da7786805ca9bc4
TanmayNankar <tanmaynankar03@gmail.com>
2021-07-28 12:26:35 +0530

    cc
    cc

--- src/Component/Employee.js
+++ src/Component/Employee.js
@@ -3,27 +3,58 @@ import React from 'react';
 class Employee extends React.Component {
     constructor() {
         super();
-        this.state = {
-            name: "Sonu",
-            salary: 10.5
-        };
+        this.state = { name: '', salary: 10.5 };
     }
-
     increaseSalary = () => {
-        console.log("Pay hiked!")
         this.setState({ salary: this.state.salary + 10 });
     };
-
+    setMyName = (evt) => {
+        this.setState({ name: evt.target.value });
+    }
     render() {
         return (<div className="App">
             <h1 > Hi! I am {this.state.name} and my salary is {this.state.salary}.</h1>
             <button type="button"
                 onClick={this.increaseSalary} >Increase</button>
+            <form>
+                <h1 style={{ color: "orange" }} >Data Form</h1>
+                <p>Name:</p>
+                <input type="text" onChange={evt => this.setMyName(evt)}></input>
+            </form>
         </div>
         );
     }
 }
 export default Employee;
+
+
+
+// import React from 'react';
+
+// class Employee extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             name: "Sonu",
+//             salary: 10.5
+//         };
+//     }
+
+//     increaseSalary = () => {
+//         console.log("Pay hiked!")
+//         this.setState({ salary: this.state.salary + 10 });
+//     };
+
+//     render() {
+//         return (<div className="App">
+//             <h1 > Hi! I am {this.state.name} and my salary is {this.state.salary}.</h1>
+//             <button type="button"
+//                 onClick={this.increaseSalary} >Increase</button>
+//         </div>
+//         );
+//     }
+// }
+// export default Employee;
 
 
 
